eb555a57bc2de4c61132542b30796378af7570dd
Charlie Graham <charlie@cvgraham.com>
2017-04-10 22:41:13 -0700

    Updated version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AlexaRequestVerifier.Mixfile do
 
   def project do
     [app: :alexa_request_verifier,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
